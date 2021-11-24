2c50440af0362ef1d61a158ece0bbfc19f0d448f
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-11-24 18:34:37 +0100

    cors fix

--- back/src/app.py
+++ back/src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import index, user
 from .db.database import BaseModel, database
@@ -8,3 +9,11 @@ BaseModel.metadata.create_all(bind=database)
 app = FastAPI()
 app.include_router(index.router)
 app.include_router(user.router, prefix="/user")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
